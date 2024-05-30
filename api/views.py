041c091a854a5ccbc992163878490415c277f53e
Elisa Ortega Girard <145747076+Elisa7584@users.noreply.github.com>
2024-05-30 13:45:18 +0200

    new: update API

--- api/views.py
+++ api/views.py
@@ -1,14 +1,19 @@
 from django.http import JsonResponse
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from django.views.decorators.csrf import csrf_exempt
-from userAuth.models import User
 
 @csrf_exempt
-@login_required
 def user_id(request):
-    user = request.user
-    user_data = {
-        'id': user.id,
-        'username': user.username,
-    }
-    return JsonResponse(user_data)
+    username = request.session.get('username')
+    if username:
+        try:
+            user = User.objects.get(username=username)
+            user_data = {
+                'id': user.id,
+                'username': user.username,
+            }
+            return JsonResponse(user_data)
+        except User.DoesNotExist:
+            return JsonResponse({'error': 'User not found'}, status=404)
+    else:
+        return JsonResponse({'error': 'User not logged in'}, status=401)
