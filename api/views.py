a997f08bd372044cf3e92baf27f2d1900d2498cb
Elisa Ortega Girard <145747076+Elisa7584@users.noreply.github.com>
2024-05-30 13:49:30 +0200

    new: API Funcional 100%

--- api/views.py
+++ api/views.py
@@ -1,11 +1,12 @@
 from django.http import JsonResponse
-from django.contrib.auth.models import User
 from django.views.decorators.csrf import csrf_exempt
+from userAuth.models import User
 
 @csrf_exempt
 def user_id(request):
     username = request.session.get('username')
     if username:
+        print(username)
         try:
             user = User.objects.get(username=username)
             user_data = {
