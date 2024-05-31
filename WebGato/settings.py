db656dc2ae8fb4f1935938387cc37d66cc9a471e
Elisa Ortega Girard <145747076+Elisa7584@users.noreply.github.com>
2024-05-31 11:59:28 +0200

    update: allowed hosts

--- WebGato/settings.py
+++ WebGato/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = 'django-insecure-ge^l+_ocw58!ojs7w-t*-6(0mi=afa2&!pn-5076mocwzzh9#z
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['webgato.onrender.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
