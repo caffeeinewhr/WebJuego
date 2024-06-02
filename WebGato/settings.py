1276d1ed62445e0152301a5b806e7c0fbdfe8947
Elisa Ortega Girard <145747076+Elisa7584@users.noreply.github.com>
2024-06-02 15:07:36 +0200

    probando cosas

--- WebGato/settings.py
+++ WebGato/settings.py
@@ -11,8 +11,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ge^l+_ocw58!ojs7w-t*-6(0mi=afa2&!pn-5076mocwzzh9#z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-ALLOWED_HOSTS = ['webgato.onrender.com', 'localhost', '127.0.0.1']
+DEBUG = True
+ALLOWED_HOSTS = []
 
 # Application definition
 
@@ -48,13 +48,13 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOW_ALL_ORIGINS = True
 
-CORS_ALLOWED_ORIGINS = [
-    'https://caffeeinewhore.itch.io',
-    'https://itch.io',
-    'https://webgato.onrender.com',
-]
+# CORS_ALLOWED_ORIGINS = [
+#     'https://caffeeinewhore.itch.io',
+#     'https://itch.io',
+#     'https://webgato.onrender.com',
+# ]
 
 ROOT_URLCONF = 'WebGato.urls'
 
