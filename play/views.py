a4c212f26cc0eb58f1afa0b8a146b3b0ccffe9f5
Elisa Ortega Girard <145747076+Elisa7584@users.noreply.github.com>
2024-06-07 11:34:46 +0200

    new a tomar porculo el login de mierda

--- play/views.py
+++ play/views.py
@@ -1,32 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from django.views.decorators.clickjacking import xframe_options_exempt
 
 @xframe_options_exempt
 def play(request):
-    username = request.session.get('username')
-    authenticated = False
-    
-    if username:
-        authenticated = True
-        context = {
-            'username': username,
-            'authenticated': authenticated
-        }
-        
-        # Use the render function to generate the response
-        response = render(request, 'itchio.html', context)
-    else:
-        context = {
-            'username': '',
-            'authenticated': authenticated
-        }
-        
-        # Use the render function to generate the response
-        response = render(request, 'beforeLog.html', context)
-    
-    # Set the necessary headers for cross-origin isolation
-    response['Cross-Origin-Opener-Policy'] = 'same-origin'
-    response['Cross-Origin-Embedder-Policy'] = 'require-corp'
-    
-    return response
+    return render(request, 'itchio.html')
